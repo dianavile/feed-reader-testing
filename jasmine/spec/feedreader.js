1a5a97e9d4dafd4ee4d77a00b864aa0e6f1e8db9
ivan_teso <ivan.teso@gmail.com>
2018-05-25 16:34:13 +0300

    Feat: test to check content if feed source change
    If another feed is loaded, the test check if the page content is
    loaded properly. Start also to fix comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,36 +1,24 @@
-/* feedreader.js
- *
+/*
  * This is the spec file that Jasmine will read and contains
  * all of the tests that will be run against your application.
  */
 
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
+
+// Tests are placed within the $() function, since some of these tests may
+// require DOM elements, to ensure they don't run until the DOM is ready.
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+
+    // Tests about RSS Feeds definition
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+
+        // Test if RSS sourced are properly defined into allFeeds array
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
-
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+        
+        // Loop into every array's object to check if the url is defined
+        // and not empty
         it('has an URL defined and not empty', function() {
             for(feed of allFeeds) {
                 expect(feed.url).toBeDefined();
@@ -103,9 +91,32 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
+
+      //let feedLinks;
+      let oldFeeds;
+      let newFeeds;
+
+          beforeEach(function(done) {
+              loadFeed(0, function() {
+
+                  oldFeeds = document.getElementsByClassName('feed')[0].innerHTML;
+                  loadFeed(1, function() {
+
+                      newFeeds = document.getElementsByClassName('feed')[0].innerHTML;
+                      done();
+                  });
+              });
+          });
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        it('is correctly loaded within .feed container', function() {
+
+            expect(newFeeds).not.toEqual(oldFeeds);
+        });
+    });
+
 }());
